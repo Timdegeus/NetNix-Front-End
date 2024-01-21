461e43c34023de18998df3d626f2fe84704322ee
Timdegeus <tim.de.geus@student.nhlstenden.com>
2024-01-21 23:55:19 +0100

    added subscription page

--- gebruikerspaneel.php
+++ gebruikerspaneel.php
@@ -27,6 +27,7 @@ exit;
                 <li><a href="accounts.php">Accounts</a></li>
                 <li><a href="movies.php">Movies</a></li>
                 <li><a href="series.php">Series</a></li>
+                <li><a href="subscription.php">Subscription</a></li>
                 <li><a href="logout.php">Uitloggen</b></a></li>
             </ul>
         </nav>
